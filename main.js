273fa3b553c8d9aecfb48e0639d5cd1dd75f7a44
MightyAlex200 <quantumtraveling@gmail.com>
2016-06-13 17:00:33 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var amount_of_windows = 0;
+function AddWindow(inp){
+  amount_of_windows += 1;
+  $('<div>',{
+      id: amount_of_windows + '_div',
+    }
+  ).appendTo('body');
+  $('#' + amount_of_windows + '_div').draggable();
+  $('<img>', {
+   src: document.getElementById(inp).value,
+   id:  amount_of_windows,
+   width: "400px",
+   frameborder: 0
+ }).appendTo('#' + amount_of_windows + '_div');
+}
